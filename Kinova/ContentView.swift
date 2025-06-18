a08538a9e493edc8b267dff6c9e87192b1a94750
Igor Camilo <11697241+igorcamilo@users.noreply.github.com>
2025-06-18 22:07:28 +0200

    Enhance project structure and add UI components
    - Updated project.pbxproj to support additional platforms.
    - Added accent colors and app icons for various devices.
    - Implemented a tabbed interface in ContentView with placeholders.

--- Kinova/ContentView.swift
+++ Kinova/ContentView.swift
@@ -9,13 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Tab("Movies", systemImage: "film") {
+                PlaceholderView("Movies") { index in
+                    Label("Movie \(index)", systemImage: "film")
+                }
+            }
+            Tab("TV Shows", systemImage: "tv") {
+                PlaceholderView("TV Shows") { index in
+                    Label("TV Show \(index)", systemImage: "tv")
+                }
+            }
+            Tab(role: .search) {
+                PlaceholderView("Search") { index in
+                    Label("Search \(index)", systemImage: "magnifyingglass")
+                }
+            }
         }
-        .padding()
+        .tabViewStyle(.sidebarAdaptable)
+        .frame(
+            minWidth: 400,
+            maxWidth: .infinity,
+            minHeight: 300,
+            maxHeight: .infinity
+        )
+    }
+}
+
+private struct PlaceholderView<RowContent: View>: View {
+    var title: LocalizedStringKey
+    var rowContent: (_ index: Int) -> RowContent
+
+    init(
+        _ title: LocalizedStringKey,
+        @ViewBuilder rowContent: @escaping (_ index: Int) -> RowContent
+    ) {
+        self.title = title
+        self.rowContent = rowContent
+    }
+
+    var body: some View {
+        NavigationStack {
+            List(1..<100, id: \.self) { index in
+                rowContent(index)
+            }
+            .navigationTitle(title)
+        }
     }
 }
 
